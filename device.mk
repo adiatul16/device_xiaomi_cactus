c5d9f27f82de03cd20e108ab8ee1485bae6a5c55
Rashed97 <rashed@linux.com>
2020-10-17 09:34:45 +0530

    cactus: Build RCS packages

--- device.mk
+++ device.mk
@@ -79,6 +79,13 @@ PRODUCT_PACKAGES += \
     fstab.mt6765 \
     ueventd.qcom.rc \
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
